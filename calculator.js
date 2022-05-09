392600afee0c955cc09b34817a24aad43cbf5038
Aisayev Duman <duman200354@mail.ru>
2022-05-10 01:50:09 +0600

    button fuctionality(numbers)

--- calculator.js
+++ calculator.js
@@ -9,7 +9,105 @@ let finish = false;
 const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
 const action = ['+', '-', '*', '/']
 
-console.log('Aruto');
+window.addEventListener('keydown', event => {
+    answerNode.value = '';
+    
+    // get pressed button
+    const key = event.key;
+
+
+    if (digits.includes(key)) {
+        if (b === '' && sign === '') {
+            a += key;
+            answerNode.value = a;
+        } else if (a !== '' && b !== '' && finish) {
+            b = key;
+            finish = false; 
+            answerNode.value = b;
+
+        } else {
+            b += key;
+            answerNode.value = b;
+        }
+        console.log(a, b, sign);
+    };
+
+    // get 
+    if (action.includes(key)) {
+        sign = key;
+        console.log(sign);
+        answerNode.value = sign;
+    };
+
+    if (key == '%') {
+        a = a/100;
+        sign = '*';
+        answerNode.value = '%';
+        // if (b !== '') {
+        //     a = a * b;
+        // } 
+        // answerNode.value = round(a);
+        return;
+    };  
+
+    // solve 
+    if (key === '=') {
+        if (b === '') b = a;
+
+        switch(sign) {
+            case '+':
+                a = (+a) + (+b);
+                break;
+
+            case '-':
+                a = (+a) - (+b);
+                break;
+
+            case '*':
+                a = (+a) * (+b);
+                break;
+                
+            case '/':
+                if (b == '0') {
+                    a = '';
+                    b = ''; 
+                    sign = '';
+                    answerNode.value = 'error';
+                    return;
+                } else {
+                    a = (+a) / (+b);
+                }
+                break;
+        }
+        finish = true;
+        answerNode.value = round(a);
+    }
+
+    if (key == 'Backspace') {
+        
+        if (b === '' || finish) {
+            a = String(a);
+            a = a.substring(0, a.length-1);
+            answerNode.value = round(a);
+            return;
+        }
+
+        b = b.substring(0, b.length-1);
+        answerNode.value = round(b);
+        
+    };
+
+    if (event.target.classList.contains('plus-minus')) {
+        if (b === '' || finish) {
+            a = -a;
+            answerNode.value = round(a);
+            return;
+        }
+        b = -b;
+        answerNode.value = round(b);  
+    };
+
+});
 
 document.querySelector('.buttons').addEventListener('click', event => {
     // pressed clear all
